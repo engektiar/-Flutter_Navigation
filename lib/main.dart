36e4856aee779f8fb56b9e3550cb081bd5894b2c
engektiar <eng.ektiar@gmail.com>
2024-01-26 22:09:38 +0600

    online theka curved navagarion add kora hoisa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:navebar/NagbationBar.dart';
 import 'package:navebar/widgets/My_Curved_Navgiton.dart';
 
 void main() {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyCurved_NavBar(),
+      home: MyCurved(),
     );
   }
 }
--- lib/widgets/My_Curved_Navgiton.dart
+++ lib/widgets/My_Curved_Navgiton.dart
@@ -2,35 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:navebar/widgets/serch.dart';
 import 'package:navebar/widgets/profile.dart';
 import 'package:navebar/widgets/homePage.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-class MyCurved_NavBar extends StatefulWidget {
-  const MyCurved_NavBar({super.key});
+class MyCurved extends StatefulWidget {
+  const MyCurved({super.key});
 
   @override
-  State<MyCurved_NavBar> createState() => _MyCurved_NavBarState();
+  State<MyCurved> createState() => _MyCurvedState();
 }
 
-class _MyCurved_NavBarState extends State<MyCurved_NavBar> {
+class _MyCurvedState extends State<MyCurved> {
   List<Widget> pages = [MyHomePage(), MyProfile(), MySearchBar()];
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          onTap: (value) {
-            setState(() {
-              currentIndex = value;
-            });
-          },
-          currentIndex: currentIndex,
-          backgroundColor: Colors.green,
-          selectedFontSize: 20,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          ]),
-    );
+        body: pages[currentIndex],
+        bottomNavigationBar: CurvedNavigationBar(
+            onTap: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            animationCurve: Curves.bounceIn,
+            items: [
+              Icon(Icons.home),
+              Icon(Icons.person),
+              Icon(Icons.search),
+            ]));
   }
 }
